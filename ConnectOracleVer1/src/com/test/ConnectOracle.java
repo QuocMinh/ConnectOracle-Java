282a6fb1b54489887b60632286427d7cc9d4e336
QuocMinh <cqminh1995@gmail.com>
2017-10-10 22:27:13 +0700

    modify something

--- ConnectOracleVer1/src/com/test/ConnectOracle.java
+++ ConnectOracleVer1/src/com/test/ConnectOracle.java
@@ -8,9 +8,16 @@ import java.sql.Statement;
 
 public class ConnectOracle {
 	
-	public static final String URL = "jdbc:oracle:thin:@127.0.0.1:1521:orcl";
+	public static final String DBNAME = "orcl";
+	public static final String URL = "jdbc:oracle:thin:@127.0.0.1:1521:" + DBNAME;
 	public static final String USERNAME = "sys as sysdba";
 	public static final String PASSWORD = "root";
+	
+	// TABLE NAME:
+	public static final String TBL_TESTORCL = "testorcl";
+	
+	// SQL: 
+	public static final String SL_ALL_TESTORCL = "SELECT * FROM " + TBL_TESTORCL; 
 
 	public static void main(String[] args) {
 		
@@ -19,21 +26,17 @@ public class ConnectOracle {
 			
 			try {
 				Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-				
 				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(SL_ALL_TESTORCL);
 				
-				ResultSet rs = stmt.executeQuery("SELECT * FROM testorcl");
-				
-				while(rs.next()) {
+				while (rs.next()) {
 					System.out.println(rs.getString(1) + " " + rs.getString(2));
 				}
 				
 			} catch (SQLException e) {
-				System.out.println("Connect failed!");
 				e.printStackTrace();
 			}
 		} catch (ClassNotFoundException e) {
-			System.out.println("Class not found");
 			e.printStackTrace();
 		}
 		
